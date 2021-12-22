483e2e72268b904c8a13dd6c3e7f9b6b5e7fe0cf
本田たか <hondataka@hondatakanoMacBook-Pro.local>
2021-12-22 17:30:50 +0900

    first commit

---
+++ src/hello/main/java/com/helloWorld/helloWorld/helloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.helloWorld.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class helloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(helloWorldApplication.class, args);
+	}
+
+}
